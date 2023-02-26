d03bb97fca1c9f035e46264d6f04ad418861a86d
Zelody <zelody@Equipo1.com>
2023-02-26 14:46:05 -0600

    Crear Lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 function App() {
+    const name = "Manuel Fernández Marquez"
+    return(
+        <div>
+            <h3>Lista de hobbies de {name}</h3>
+            <ul>
+                <li>Jugar Videojuegos</li>
+                <li>Escuchar Música</li>
+                <li>Cocinar</li>
+            </ul>
+        </div>
+    )
 
 }
 
